0b68e4d6e24210f9c3f70b8b668f3023f5c29cc6
nickolai-kolin <nickolai.zt@gmail.com>
2021-02-15 15:44:25 +0200

    Added HTML5 player

--- dist/player.js
+++ dist/player.js
@@ -1,19 +1,35 @@
 let player;
-const playerWrap = document.querySelector('.player');
-const playerStart = document.querySelector('.player__start');
-const playerVideoDuration = document.querySelector('.player__time-duration');
-const playerVideoCurrent = document.querySelector('.player__time-current');
+const playerWrap = document.querySelector(".player");
+const playerStart = document.querySelectorAll(".player__start");
+const playerVideoDuration = document.querySelector(".player__time-duration");
+const playerVideoCurrent = document.querySelector(".player__time-current");
+const playerPlayback = document.querySelector(".player__playback");
+const playerMarker = document.querySelector(".player__marker");
+
+const playerVolumeMarker = document.querySelector(".player__volumemarker");
+const playerVolume = document.querySelector(".player__volume");
 
 function eventsInit() {
-  playerStart.addEventListener('click', ()=>{
-    if (!playerWrap.classList.contains('player--paused')){
-      player.pauseVideo();
-      playerWrap.classList.add('player--paused')
-    } else {
-      player.playVideo();
-      playerWrap.classList.remove('player--paused');
+  Array.from(playerStart).forEach((el) => {
+    el.addEventListener("click", (ev) => {
+      const currentState = player.getPlayerState();
+      if (currentState == 1) {player.pauseVideo()};
+      if (currentState == 2 || currentState == 0 || currentState == 5) {
+        player.playVideo();}
+    });
+  });
+  playerPlayback.addEventListener("click", (ev) => {
+    const newProgress = (100 * ev.layerX) / playerPlayback.clientWidth;
+    playerMarker.style.left = `${newProgress}%`;
+    const newCurrentSec = Math.floor(
+      (newProgress * player.getDuration()) / 100
+    );
+    player.seekTo(newCurrentSec);
+  });
 
-    }
+  playerVolume.addEventListener('click', (ev)=>{
+    const newVolume = (100 * ev.layerX) / playerVolume.clientWidth;
+    setPlayerVolume(newVolume);
   });
 }
 
@@ -25,48 +41,79 @@ function converTime(sec) {
   let ss = Math.trunc(sec % 60);
 
   function _formatPeriod(t) {
-    // Check 
-    return t < 10 ? `0${t}` : `${t}`
+    // Check
+    return t < 10 ? `0${t}` : `${t}`;
   }
-
-  return `${_formatPeriod(hh)}:${_formatPeriod(mm)}:${_formatPeriod(ss)}`
+  let resultFormat = hh > 0 ? `${_formatPeriod(hh)}:${_formatPeriod(mm)}:${_formatPeriod(ss)}` : `${_formatPeriod(mm)}:${_formatPeriod(ss)}`
+  return `${_formatPeriod(mm)}:${_formatPeriod(ss)}`;
 }
 
-function onPlayerReady(){
+function setPlayerVolume(vol){
+  // vol: number
+  player.setVolume(Math.floor(vol));
+  playerVolumeMarker.style.left = `${vol}%`;
+}
+
+function onPlayerReady(ev) {
   let interval;
   const videoDurationSec = player.getDuration();
   playerVideoDuration.textContent = converTime(videoDurationSec);
 
-  if (typeof(interval) !== undefined){
+  if (typeof interval !== undefined) {
     clearInterval(interval);
   }
 
-  interval = setInterval(()=>{
+  interval = setInterval(() => {
+    // Setting current time
     const videoCurrentSec = player.getCurrentTime();
     playerVideoCurrent.textContent = converTime(videoCurrentSec);
+    // Setting marker on progress bar
+    const progress = (100 * videoCurrentSec) / videoDurationSec;
+    playerMarker.style.left = `${progress}%`;
   }, 1000);
-
+  // Volume
+  setPlayerVolume(player.getVolume());
+  
 }
 
-function onYouTubeIframeAPIReady(){
-  player = new YT.Player('player',{
+function onPlayerStateChange(ev) {
+  /*
+    -1 (unstarted)
+    0 (ended)
+    1 (playing)
+    2 (paused)
+    3 (buffering)
+    5 (video cued).
+  */
+  switch (ev.data) {
+    case 1:
+      playerWrap.classList.remove("player--paused");
+      break;
+    case 2:
+      playerWrap.classList.add("player--paused");
+      break;
+    case 0: // end
+      playerWrap.classList.add("player--paused");
+  }
+}
+
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player("player", {
     width: "662",
     height: "392",
-    videoId: "RBSGKlAvoiM",
+    videoId: "Og847HVwRSI",
     events: {
       onReady: onPlayerReady,
+      onStateChange: onPlayerStateChange,
     },
     playerVars: {
       controls: 0,
       disablekb: 1,
       enablejsapi: 1,
       modestbranding: 1,
-      rel: 0
-      
-    }
-
+      rel: 0,
+    },
   });
 }
-
 
 eventsInit();
